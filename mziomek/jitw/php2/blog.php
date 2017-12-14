010d69953cfe552779936d5a79a9deb8783cfaa4
Mateusz Ziomek <mziomek108@gmail.com>
2017-12-14 01:07:26 +0100

    up

--- mziomek/jitw/php2/blog.php
+++ mziomek/jitw/php2/blog.php
@@ -29,7 +29,6 @@
         echo "Blog $name";
         
         echo "<br><br>Opis:";
-        echo "<br><br>Opis:";
         $file = fopen($name . '/info.txt', 'r');
         $s = fgets($file);
         $s = fgets($file);
